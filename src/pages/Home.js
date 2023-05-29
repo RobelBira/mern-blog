21544ab97a0c2a485e8c8032cca54e861a7b0c6a
RobelBira <robelbira@gmail.com>
2023-05-29 15:29:33 +0300

    commit-1

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+<h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+    Hello, welcome to my blog.
+    </h1>
+<p className="mx-auto leading-relaxed text-base mb-4">Irure tempor et excepteur eu in voluptate dolore nostrud incididunt occaecat proident aliquip culpa do. Laboris nulla tempor Lorem fugiat eu. Dolor aute dolore ea exercitation non. Ut non mollit ex exercitation. Veniam fugiat amet mollit ex elit reprehenderit aute exercitation excepteur nostrud nostrud excepteur dolore pariatur.
+    </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Irure tempor et excepteur eu in voluptate dolore nostrud incididunt occaecat proident aliquip culpa do. Laboris nulla tempor Lorem fugiat eu. Dolor aute dolore ea exercitation non. Ut non mollit ex exercitation. Veniam fugiat amet mollit ex elit reprehenderit aute exercitation excepteur nostrud nostrud excepteur dolore pariatur.
+    </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Irure tempor et excepteur eu in voluptate dolore nostrud incididunt occaecat proident aliquip culpa do. Laboris nulla tempor Lorem fugiat eu. Dolor aute dolore ea exercitation non. Ut non mollit ex exercitation. Veniam fugiat amet mollit ex elit reprehenderit aute exercitation excepteur nostrud nostrud excepteur dolore pariatur.
+    Irure tempor et excepteur eu in voluptate dolore nostrud incididunt occaecat proident aliquip culpa do. Laboris nulla tempor Lorem fugiat eu. Dolor aute dolore ea exercitation non. Ut non mollit ex exercitation. Veniam fugiat amet mollit ex elit reprehenderit aute exercitation excepteur nostrud nostrud excepteur dolore pariatur.
+    </p>
+    </div>
+    
+  )
+}
+export default Home
